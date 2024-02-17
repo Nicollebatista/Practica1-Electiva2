63717597f4494042af431b97850a350ff4b31b9e
Nicollebatista <nicolveiker@gmail.com>
2024-02-17 09:58:06 -0400

    Se agrega el proyecto

---
+++ ApplicationBdContext.cs
@@ -0,0 +1,20 @@
+﻿using ForoEFapi.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForoEFapi
+{
+    public class ApplicationBdContext : DbContext
+    {
+        public ApplicationBdContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuario>().HasKey(g => g.Id);
+           modelBuilder.Entity<Usuario>().Property(g => g.Nombre).HasMaxLength(30);
+        }
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForoEFapi.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        //[StringLength(maximumLength: 30)]
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public string Carrera { get; set; } = null!;
+    }
+}
